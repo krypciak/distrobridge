16ea1e55700d868a1049e360f026fc2466337b9a
krypek <krypek@tuta.io>
2023-05-27 22:52:27 +0200

    Create script only on configure

--- packages/social.sh
+++ packages/social.sh
@@ -7,18 +7,18 @@ artix_social_install() {
 arch_social_install() {
     echo 'betterdiscordctl discord noto-fonts-cjk noto-fonts-emoji ttf-symbola'
 }
-echo "
-if  ping -c 1 gnu.org > /dev/null 2>&1 && [ \"$MODE\" = 'iso' ]; then
-    set +e
-    timeout 15s xvfb-run -a discord > /dev/null 2>&1
-    betterdiscordctl install > /dev/null 2>&1
-    set -e
-fi
-" > /tmp/discordscript
 
 _configure_discord() {
+    echo "
+    if  ping -c 1 gnu.org > /dev/null 2>&1 && [ \"$MODE\" = 'iso' ]; then
+        set +e
+        timeout 15s xvfb-run -a discord > /dev/null 2>&1
+        betterdiscordctl install > /dev/null 2>&1
+        set -e
+    fi
+    " > "$REPOHUB"/tmp/discordscript
     # Let discord download updates
-    doas -u $USER1 timeout 40s sh -c /tmp/discordscript &
+    doas -u $USER1 timeout 40s sh -c "$REPOHUB"/tmp/discordscript &
 }
 
 artix_social_configure() {
