b0c83b4b225ed520fe5f2538a12faa105097b8f6
krypek <krypek@tuta.io>
2024-05-21 10:48:27 +0200

    Add typescript-language-server

--- packages/coding.sh
+++ packages/coding.sh
@@ -2,11 +2,11 @@
 
 # github-desktop-bin
 artix_coding_install() {
-    echo 'esbuild git-filter-repo github-cli jdk17-openjdk jdk8-openjdk jdk-openjdk rust shellcheck'
+    echo 'esbuild git-filter-repo github-cli jdk17-openjdk jdk8-openjdk jdk-openjdk rust shellcheck typescript-language-server'
 }
 
 arch_coding_install() {
-    echo 'esbuild git-filter-repo github-cli jdk17-openjdk jdk8-openjdk jdk-openjdk rust shellcheck'
+    echo 'esbuild git-filter-repo github-cli jdk17-openjdk jdk8-openjdk jdk-openjdk rust shellcheck typescript-language-server'
 }
 
 artix_coding_configure() {
